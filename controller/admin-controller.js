37a8d66af86e3fd1bdc6a3c4a66f4b826b98e75c
unknown <dickensallinone@gmail.com>
2024-10-20 18:20:14 +0800

    add more functionalities on the backend for admin page data

--- controller/admin-controller.js
+++ controller/admin-controller.js
@@ -1,6 +1,46 @@
 const { QueryTypes } = require("sequelize");
-
+const sequelize = require("../db/dbconnection");
+const Appointment = require("../model/Appointment");
+const UserProfile = require("../model/UserProfile");
 const adminHeaderCardsDetails = async( req,res,next)=>{
+    try{
+
+        const adminCardDetails = await sequelize(
+            `select (select count(e.userId) UserProfile e
+            where e.userType <> 'admin')
+            as users ,(select count(e.appointmentId) from Appointment e)
+            as assistance from UserProfile f
+            where f.userType = 'admin' limit 1`,
+            {type:QueryTypes.SELECT}
+        )
+
+        res.status(200).send({
+            isSuccess: true,
+            message: "Successfully retrieve Admin Dashboard Details",
+            data: adminCardDetails
+        })
+
+    }catch(e){
+        next(e)
+    }
+}
+
+const manageRatings = async( req,res,next)=>{
+    try{    
+
+
+        res.status(200).send({
+            isSuccess: true,
+            message: "Successfully remove ratings",
+            
+        })
+
+    }catch(e){
+        next(e)
+    }
+}
+
+const showRatings = async(req,res,next)=>{
     try{
 
     }catch(e){
@@ -8,6 +48,44 @@ const adminHeaderCardsDetails = async( req,res,next)=>{
     }
 }
 
+const manageUsers = async(req,res,next)=>{
+    try{
+
+    }catch(e){
+        next(e)
+    }
+}
+
+const showUsers = async(req,res,next)=>{
+    try{
+
+    }catch(e){
+        next(e)
+    }
+}
+
+const showPendingListOfAssistantAccountApplication = async(req,res,next)=>{
+    try{
+
+    }catch(e){
+        next(e)
+    }
+}
+
+const validateAssistantAccountRegisteration = async(req,res,next)=>{
+    try{
+
+    }catch(e){
+        next(e)
+    }
+}
+
+
 module.exports = {
-    adminHeaderCardsDetails
+    adminHeaderCardsDetails,
+    manageRatings,
+    showRatings,
+    manageUsers,
+    showUsers,showPendingListOfAssistantAccountApplication,
+    validateAssistantAccountRegisteration
 }
--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const auth = require("../auth/auth");
-const { adminHeaderCardsDetails } = require('../controller/admin-controller');
+const { adminHeaderCardsDetails, 
+    showUsers, showRatings, 
+    showPendingListOfAssistantAccountApplication,
+     manageUsers, manageRatings, 
+     validateAssistantAccountRegisteration } = require('../controller/admin-controller');
 
 const router = express.Router();
 
 router.get("/admin-cards/details",auth.verify,adminHeaderCardsDetails)
 
+router.get("/user-list",auth.verify
+    ,showUsers
+);
+
+router.get("/ratings-list",auth.verify,showRatings);
+
+
+router.get("/assistant-applicants",auth.verify,showPendingListOfAssistantAccountApplication);
+
+router.put("/user-manage/:userId",auth.verify,manageUsers);
+
+router.put("/ratings-manage/:ratingId",auth.verify,manageRatings);
+
+router.put("/assistant-applicant/:applicantId",auth.verify,validateAssistantAccountRegisteration);
 
 module.exports = router;
